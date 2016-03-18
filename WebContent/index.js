e95f19ca1b1c354602e3f579aca4a92316b291a7
Mike <michael.mathews10@comcast.net>
2016-03-17 22:13:39 -0400

    Deleted rows
    deleted rows of owners

---
+++ WebContent/index.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+var numTeams, leagueName, leagueID;
+
+function getNumTeams() {
+	var numTeams = $( "#teamSelect" ).val();
+	console.log(numTeams)
+}
+
+function createLeague(){
+	numTeams = $( "#teamSelect" ).val();
+	leagueName = $("leagueName").val();
+	leagueID = 1;
+	var league = [{"num": numTeams, "name": leagueName, "id": leagueID}];
+	console.log(league);
+	$.ajax({
+	    type: 'get', // it's easier to read GET request parameters
+	    url: 'index.js',
+	    dataType: 'JSON',
+	    data: { 
+	      league: JSON.stringify(league) // look here!
+	    },
+	    success: function(data) {
+	    	console.log("got here");
+	    },
+	    error: function(data) {
+	        console.log(data.num);
+	    }
+	});
+}
