cfa45cd51a20382b83be093da5e92d9beef4de59
Mike <michael.mathews10@comcast.net>
2016-10-01 16:17:39 -0400

    DTP setup

--- WebContent/js/createLeague.js
+++ WebContent/js/createLeague.js
@@ -10,25 +10,4 @@ document.getElementById("joinLeague").addEventListener("click", function(){
 	window.location = "http://localhost:8080/playerDashboard.html"; 
 	});
 
-//var numTeams, leagueName, leagueID;
-//
-//function getNumTeams() {
-//	var numTeams = $( "#teamSelect" ).val();
-//	console.log(numTeams)
-//}
-//function leagueScreen(){
-//	window.location = "http://localhost:8080/Dashboard.html";
-//}
-//function createLeague(){
-//	
-//	numTeams = $( "#teamSelect" ).val();
-//	leagueName = $("#leagueName").val();
-//	var xhr = new XMLHttpRequest();
-//    xhr.onreadystatechange = function() {
-//        if (xhr.readyState == 4 && xhr.status == 200) {
-//        	leagueScreen();
-//        }
-//    }
-//    xhr.open('GET', '/App?league_name='+leagueName+'&num_teams='+numTeams, true);
-//    xhr.send(null);
-//}
+
