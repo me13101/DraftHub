619f4fc1b9f14c06fc775aae7c5b65d020f5dd84
Mike <michael.mathews10@comcast.net>
2016-10-22 13:21:49 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -13,24 +13,24 @@
 //     }
 
 //current home.js angular
-//var home = angular.module('home', []);
-//
-//home.controller('homeController', function ($scope) {
-//    $.ajax({
-//        url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
-//        success: function(result){
-//            $scope.news = result.sources;
-//            $scope.$apply();
-//        }});
-//});
+var home = angular.module('home', []);
 
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
-  $http.get("https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988")
-  .then(function(response) {
-      $scope.newsArticles = response.sources;
-  });
+home.controller('homeController', function ($scope) {
+    $.ajax({
+        url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
+        success: function(result){
+            $scope.news = result.data;
+            $scope.$apply();
+        }});
 });
+
+//var app = angular.module('myApp', []);
+//app.controller('myCtrl', function($scope, $http) {
+//  $http.get("https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988")
+//  .then(function(response) {
+//      $scope.newsArticles = response.data;
+//  });
+//});
 
  var host = window.location.host;
  function getURL(res){
