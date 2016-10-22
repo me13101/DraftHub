e1388d6fcb42a3153a38154ea9821e8d1050862c
Mike <michael.mathews10@comcast.net>
2016-10-22 13:09:06 -0400

    add home.js

--- WebContent/js/nav.js
+++ WebContent/js/nav.js
@@ -11,20 +11,20 @@ document.getElementById("joinLeague").addEventListener("click", function(){
 	});
 
 
-function newsapi() {
-        $.ajax({
-            url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
-            context: document.getElementById("news"),
-            success: function(result){
-                        newsTable(result);
-                    }});
-    console.log("got here");
-}
-
-function newsTable(result){
-    for (var i = 0; i < result.sources.length; i++){
-        document.getElementById("newsTable").insertRow(i).innerHtml = result.sources[i].name;
-    }
+//function newsapi() {
+//        $.ajax({
+//            url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
+//            context: document.getElementById("news"),
+//            success: function(result){
+//                        newsTable(result);
+//                    }});
+//    console.log("got here");
+//}
+//
+//function newsTable(result){
+//    for (var i = 0; i < result.sources.length; i++){
+//        document.getElementById("newsTable").insertRow(i).innerHtml = result.sources[i].name;
+//    }
 }
 //var numTeams, leagueName, leagueID;
 //
