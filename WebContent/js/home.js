bc5d26165cb3e588aa83ef65f7b4404d71c0fabe
Mike <michael.mathews10@comcast.net>
2016-10-22 13:20:32 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -28,7 +28,7 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
   $http.get("https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988")
   .then(function(response) {
-      $scope.myWelcome = response.data;
+      $scope.newsArticles = response.data;
   });
 });
 
