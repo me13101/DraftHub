1d9551f23427c99a741d9146caf79ed9f239bdcf
Mike <michael.mathews10@comcast.net>
2016-10-22 13:13:36 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -12,16 +12,26 @@
 //         //console.log("got here");
 //     }
 
-var home = angular.module('home', []);
+//current home.js angular
+//var home = angular.module('home', []);
+//
+//home.controller('homeController', function ($scope) {
+//    $.ajax({
+//        url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
+//        success: function(result){
+//            $scope.news = result.sources;
+//            $scope.$apply();
+//        }});
+//});
 
-home.controller('homeController', function ($scope) {
-    $.ajax({
-        url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
-        success: function(result){
-            $scope.news = result.sources;
-            $scope.$apply();
-        }});
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+  $http.get("https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988")
+  .then(function(response) {
+      $scope.myWelcome = response.data;
+  });
 });
+
  var host = window.location.host;
  function getURL(res){
    if (res == "home"){
