c7e7b6b5dbb6e31d59c2a587bbec279cf813fc6c
Mike <michael.mathews10@comcast.net>
2016-10-22 12:26:36 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -15,7 +15,7 @@
 var home = angular.module('home', []);
 
 home.controller('homeController', function ($scope) {
-    $scope.news = [];
+    $scope.news = [{name="NFL.com"},{name = "Test Article"}];
     $.ajax({
         url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
         context: document.getElementById("news"),
