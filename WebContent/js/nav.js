4f5a1d53533a3915d5bbb7f446fe990134848c62
Mike <michael.mathews10@comcast.net>
2016-10-22 14:55:20 -0400

    add home.js

--- WebContent/js/nav.js
+++ WebContent/js/nav.js
@@ -3,9 +3,9 @@
  */
 
 document.getElementById("createLeague").addEventListener("click", function(){
-	window.location = "http://localhost:8080/commCreateLeague.html";
+	window.location = "http://me13101.github.io/WebContent/commCreateLeague.html";
 	});
 
 document.getElementById("joinLeague").addEventListener("click", function(){
-	window.location = "http://localhost:8080/playerDashboard.html"; 
+	window.location = "http://me13101.github.io/WebContent/playerDashboard.html"; 
 	});
