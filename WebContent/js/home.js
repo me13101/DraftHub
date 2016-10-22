1cc4eb6499e40c8c934999eb1d8cba1aa023fe42
Mike <michael.mathews10@comcast.net>
2016-10-22 12:29:48 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -13,9 +13,9 @@
 //     }
 
 var home = angular.module('home', []);
-
+$scope.news = ["NFL.com","Test Article"];
 home.controller('homeController', function ($scope) {
-    $scope.news = ["NFL.com","Test Article"];
+    //$scope.news = ["NFL.com","Test Article"];
     $.ajax({
         url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
         context: document.getElementById("news"),
