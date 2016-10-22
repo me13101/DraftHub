0eecb4f11eda621436862363a3051728d0f92bbb
Mike <michael.mathews10@comcast.net>
2016-10-21 22:50:20 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -2,14 +2,8 @@
  * 
  */
 
+document.onload = newsapi();
 
- var host = window.location.host;
- var result;
- function getURL(res){
-   if (res == "home"){
-       return host+"/leagueDashboard.html";
-   }
-}
 function newsapi() {
     $.ajax({
         url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
@@ -19,6 +13,7 @@ function newsapi() {
         }});
         //console.log("got here");
     }
+
 function ang(result){
     var home = angular.module('home');
     try{
@@ -31,3 +26,10 @@ function ang(result){
     }
 }
 
+ var host = window.location.host;
+ var result;
+ function getURL(res){
+   if (res == "home"){
+       return host+"/leagueDashboard.html";
+   }
+}
