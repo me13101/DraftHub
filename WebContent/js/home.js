57e0d23cc94594d753f1b12e16ba1f78654214c6
Mike <michael.mathews10@comcast.net>
2016-10-21 22:18:19 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -16,18 +16,6 @@ var home = angular.module('home', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 home.controller('home', function PhoneListController($scope) {
-  $scope.phones = [
-    {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
-    }
-  ];
 });
 var host = window.location.host;
 var result;
@@ -42,16 +30,16 @@ var result;
                 url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
                 context: document.getElementById("news"),
                 success: function(result){
-                            newsTable(result);
+                           //newsTable(result);
                         }});
         //console.log("got here");
     }
 
-    function newsTable(result){
-        for (var i = 0; i < result.sources.length; i++){
-            document.getElementById("newsTable").insertRow(i).innerHtml = result.sources[i].name;
-        }
-    }
+//    function newsTable(result){
+//        for (var i = 0; i < result.sources.length; i++){
+//            document.getElementById("newsTable").insertRow(i).innerHtml = result.sources[i].name;
+//        }
+//    }
 //test
 //var numTeams, leagueName, leagueID;
 //
