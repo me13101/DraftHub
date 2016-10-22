af75b0fb4ab773e8dad1e73b90b933e3b9760fb7
Mike <michael.mathews10@comcast.net>
2016-10-21 22:23:27 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -3,20 +3,6 @@
  */
 
 
-//document.getElementById("createLeague").addEventListener("click", function(){
-//    newsapi();
-//	window.location = "http://localhost:8080/commCreateLeague.html";
-//	});
-//
-//document.getElementById("joinLeague").addEventListener("click", function(){
-//	window.location = "http://localhost:8080/playerDashboard.html";
-//	});
-// Define the `phonecatApp` module
-var home = angular.module('home', []);
-
-// Define the `PhoneListController` controller on the `phonecatApp` module
-home.controller('home', function PhoneListController($scope) {
-});
 var host = window.location.host;
 var result;
 	function getURL(res){
@@ -34,32 +20,10 @@ var result;
                         }});
         //console.log("got here");
     }
+    var home = angular.module('home', []);
 
-//    function newsTable(result){
-//        for (var i = 0; i < result.sources.length; i++){
-//            document.getElementById("newsTable").insertRow(i).innerHtml = result.sources[i].name;
-//        }
-//    }
-//test
-//var numTeams, leagueName, leagueID;
-//
-//function getNumTeams() {
-//	var numTeams = $( "#teamSelect" ).val();
-//	console.log(numTeams)
-//}
-//function leagueScreen(){
-//	window.location = "http://localhost:8080/Dashboard.html";
-//}
-//function createLeague(){
-//	
-//	numTeams = $( "#teamSelect" ).val();
-//	leagueName = $("#leagueName").val();
-//	var xhr = new XMLHttpRequest();
-//    xhr.onreadystatechange = function() {
-//        if (xhr.readyState == 4 && xhr.status == 200) {
-//        	leagueScreen();
-//        }
-//    }
-//    xhr.open('GET', '/App?league_name='+leagueName+'&num_teams='+numTeams, true);
-//    xhr.send(null);
-//}
+    // Define the `PhoneListController` controller on the `phonecatApp` module
+    home.controller('home', function ($scope) {
+    $scope = newsapi();
+    });
+
