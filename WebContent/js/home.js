93bb32a8e57ae13420f87a87a8189a0d753f82d3
Mike <michael.mathews10@comcast.net>
2016-10-21 22:34:00 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -3,27 +3,28 @@
  */
 
 
-var host = window.location.host;
-var result;
-	function getURL(res){
-	    if (res == "home"){
-	    return host+"/leagueDashboard.html";
-	    }
-	}
-	document.onload = newsapi();
-	function newsapi() {
-            $.ajax({
-                url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
-                context: document.getElementById("news"),
-                success: function(result){
-                           //newsTable(result);
-                        }});
+ var host = window.location.host;
+ var result;
+ function getURL(res){
+   if (res == "home"){
+       return host+"/leagueDashboard.html";
+   }
+}
+document.onload = newsapi();
+function newsapi() {
+    $.ajax({
+        url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
+        context: document.getElementById("news"),
+        success: function(result){
+            ang(result);
+        }});
         //console.log("got here");
     }
+function ang(result){
     var home = angular.module('home', []);
-
-    // Define the `PhoneListController` controller on the `phonecatApp` module
+    
     home.controller('home-controller', function ($scope) {
     $scope = newsapi();
     });
+}
 
