35f074e9390fed632bc69433bb27040d21636551
Mike <michael.mathews10@comcast.net>
2016-10-22 12:38:05 -0400

    add home.js

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -13,17 +13,16 @@
 //     }
 
 var home = angular.module('home', []);
-$.ajax({
+
+home.controller('homeController', function ($scope) {
+    $.ajax({
         url: "https://newsapi.org/v1/sources?source=the-next-web&sortBy=latest&apiKey=ca73649cfc624eaf975ffa109305e988",
         context: document.getElementById("news"),
         success: function(result){
-            var news = result.sources;
+            $scope.news = result;
+            $scope.$applyAsync();
         }});
-home.controller('homeController', function ($scope, news) {
-    $scope.news = news;
-
 });
-
  var host = window.location.host;
  function getURL(res){
    if (res == "home"){
